d370704b8636c72a6384b000f2ec9d871286b804
Dave Bouman <49709450+DaveBouman@users.noreply.github.com>
2022-04-10 09:53:13 +0200

    Changes package files

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -1,4 +1,5 @@
 import { Todo } from '../entities/database/todo';
+import { Request, Response } from 'express';
 import BaseController from './baseController';
 
 class TodoController extends BaseController<Todo> {
@@ -11,6 +12,18 @@ class TodoController extends BaseController<Todo> {
     /*
     example function on how to override from base implementation
     */
+
+    override getList = async (req: Request, res: Response) => {
+        const skip = req.query.skip as unknown as number;
+        const take = req.query.take as unknown as number;
+
+
+        return res.send({
+            message: "This is succesfull",
+            entity: 'entities',
+        });
+    };
+
 
     // override getOneById = async (req: Request, res: Response) => {
     //     const id = req.params.id as unknown as number;
