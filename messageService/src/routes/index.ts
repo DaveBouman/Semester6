d38951bc456948d518900a0bf1ab94d1ec5c6472
Dave Bouman <49709450+DaveBouman@users.noreply.github.com>
2022-04-14 07:41:25 +0200

    Beginning of dapr

--- messageService/src/routes/index.ts
+++ messageService/src/routes/index.ts
@@ -1,19 +1,9 @@
 import { useErrorHandler as use } from '../middleware/apiErrorMiddleware';
 import { Router, } from 'express';
-import Google from './google';
-import Facebook from './facebook';
-import MagicLogin from './magicLogin';
-import Stripe from './stripe';
-import Todo from './todo';
 import Message from './message';
 
 const routes = Router();
 
-routes.use('/google', Google);
-routes.use('/todos', Todo);
-routes.use('/facebook', use(Facebook));
-routes.use('/auth', MagicLogin);
-routes.use('/stripe', Stripe);
 routes.use('/messages', Message);
 
 export default routes;
