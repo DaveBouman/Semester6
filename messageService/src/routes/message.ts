7fd6c4ce24c0f3d845a00cf4caa560c6eeb20c70
Dave Bouman <49709450+DaveBouman@users.noreply.github.com>
2022-04-14 06:39:28 +0200

    Added dapr

--- messageService/src/routes/message.ts
+++ messageService/src/routes/message.ts
@@ -4,6 +4,6 @@ import MessageController from '../controllers/messageController';
 const router = Router();
 const messageController = new MessageController();
 
-router.get("/event", messageController.createTopic);
+router.post("/", messageController.create);
 
 export default router;
